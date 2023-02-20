4604c2e57d3915b54c56309afa178b46154d8617
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2023-02-20 11:28:12 +0600

    MainActivity drawer toolbar my order fragment

--- app/src/main/java/com/example/signify/MainActivity.kt
+++ app/src/main/java/com/example/signify/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.signify
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.DataBindingUtil
+import com.example.signify.databinding.ActivityMainBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
 
 class MainActivity : AppCompatActivity() {
+
+    //#1 Defining a BottomSheetBehavior instance
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // Inflate the content view (replacing `setContentView`)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        setSupportActionBar(binding.myToolbar)
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.myToolbar, R.string.app_name, R.string.app_name)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        //#2 Initializing the BottomSheetBehavior
+        bottomSheetBehavior = BottomSheetBehavior.from(binding.test1.bottomSheet)
+        //#3 Listening to State Changes of BottomSheet
+        bottomSheetBehavior.addBottomSheetCallback(object :
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                binding.buttonBottomSheetPersistent.text = when (newState) {
+                    BottomSheetBehavior.STATE_EXPANDED -> "Close Persistent Bottom Sheet"
+                    BottomSheetBehavior.STATE_COLLAPSED -> "Open Persistent Bottom Sheet"
+                    else -> "Persistent Bottom Sheet"
+                }
+            }
+        })
+
+
+        //#4 Changing the BottomSheet State on ButtonClick
+        binding.buttonBottomSheetPersistent.setOnClickListener {
+            val state =
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HALF_EXPANDED)
+                    BottomSheetBehavior.STATE_COLLAPSED
+                else
+                    BottomSheetBehavior.STATE_HALF_EXPANDED
+            bottomSheetBehavior.state = state
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
