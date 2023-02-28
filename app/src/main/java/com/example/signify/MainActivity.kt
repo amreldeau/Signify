f17f9d19afde7a376af387bbc2a182a74aa45100
Nurzhan Mussabekov <200107138@stu.sdu.edu.kz>
2023-02-28 21:46:05 +0600

    main page design changes

--- app/src/main/java/com/example/signify/MainActivity.kt
+++ app/src/main/java/com/example/signify/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.example.signify
 
 import android.os.Bundle
-import android.view.Menu
 import android.view.MenuItem
 import android.view.View
+import android.view.View.OnFocusChangeListener
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import com.example.signify.databinding.ActivityMainBinding
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MapStyleOptions
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 
@@ -18,19 +24,39 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
     private lateinit var binding: ActivityMainBinding
     private lateinit var toggle: ActionBarDrawerToggle
+    val almaty = LatLng(43.24, 76.88)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Inflate the content view (replacing `setContentView`)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        val mapFragment = supportFragmentManager.findFragmentById(
+            R.id.map_fragment
+        ) as? SupportMapFragment
+        mapFragment?.getMapAsync { googleMap ->
+            onMapReady(googleMap)
+            googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(applicationContext, R.raw.map_style));
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(almaty, 15f))
+            googleMap.setOnCameraMoveListener {
+                bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+            }
+        }
+
+
+
+
+
         setSupportActionBar(binding.myToolbar)
+        supportActionBar?.title = "";
         toggle = ActionBarDrawerToggle(this, binding.drawerLayout, binding.myToolbar, R.string.app_name, R.string.app_name)
         binding.drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
 
         //#2 Initializing the BottomSheetBehavior
         bottomSheetBehavior = BottomSheetBehavior.from(binding.test1.bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_HIDDEN
+
         //#3 Listening to State Changes of BottomSheet
         bottomSheetBehavior.addBottomSheetCallback(object :
             BottomSheetBehavior.BottomSheetCallback() {
@@ -39,8 +65,8 @@ class MainActivity : AppCompatActivity() {
 
             override fun onStateChanged(bottomSheet: View, newState: Int) {
                 binding.buttonBottomSheetPersistent.text = when (newState) {
-                    BottomSheetBehavior.STATE_EXPANDED -> "Close Persistent Bottom Sheet"
-                    BottomSheetBehavior.STATE_COLLAPSED -> "Open Persistent Bottom Sheet"
+                    BottomSheetBehavior.STATE_HIDDEN -> "is hidden"
+                    BottomSheetBehavior.STATE_EXPANDED -> "is open"
                     else -> "Persistent Bottom Sheet"
                 }
             }
@@ -50,10 +76,10 @@ class MainActivity : AppCompatActivity() {
         //#4 Changing the BottomSheet State on ButtonClick
         binding.buttonBottomSheetPersistent.setOnClickListener {
             val state =
-                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HALF_EXPANDED)
-                    BottomSheetBehavior.STATE_COLLAPSED
-                else
+                if (bottomSheetBehavior.state == BottomSheetBehavior.STATE_HIDDEN)
                     BottomSheetBehavior.STATE_HALF_EXPANDED
+                else
+                    BottomSheetBehavior.STATE_HIDDEN
             bottomSheetBehavior.state = state
         }
     }
@@ -63,5 +89,16 @@ class MainActivity : AppCompatActivity() {
             return true
         }
         return super.onOptionsItemSelected(item)
+    }
+    /**
+     * Adds marker representations of the places list on the provided GoogleMap object
+     */
+    private fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
     }
 }
