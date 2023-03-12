54c5ac5a8b3df9c60dc411883dc90ac48fda0972
Yeldos <eldos.amire@gmail.com>
2023-03-13 00:07:30 +0600

    Wrapped entry text at the bottom of each activity in LinearLayout. Then linked two activities

--- app/src/main/java/com/example/signify/MainActivity.kt
+++ app/src/main/java/com/example/signify/MainActivity.kt
@@ -1,14 +1,32 @@
 package com.example.signify
 
+import android.content.Intent
+import android.icu.lang.UCharacter.GraphemeClusterBreak.V
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.text.HtmlCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         //force app to always use the light theme
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+
+        //START:[Enter Sign Up page] This code sets an OnClickListener on the TextView, so when it is clicked, it will start the SignUp
+        val signUpEntry = findViewById<TextView>(R.id.sign_up_entry)
+        signUpEntry.setOnClickListener {
+            val intent = Intent(this, SignUp::class.java)
+            startActivity(intent)
+        }
+        //END:[Enter Sign Up page]
     }
 }
--- app/src/main/java/com/example/signify/SignUp.kt
+++ app/src/main/java/com/example/signify/SignUp.kt
@@ -1,11 +1,27 @@
 package com.example.signify
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
+import android.view.View
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 
 class SignUp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        //START:[Enter Sign Up page] This code sets an OnClickListener on the TextView, so when it is clicked, it will start the SignUp
+        val logInEntry = findViewById<TextView>(R.id.log_in_entry)
+        logInEntry.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+        //END: Enter MainActivity
     }
 }
